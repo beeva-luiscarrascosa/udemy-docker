3b0de171dc5c386693fcf6e6fceb81c51bb8d7ed
beeva-luiscarrascosa <luis.carrascosa@beeva.com>
2018-04-28 09:50:56 +0200

    mas ejercicio resueltos

---
+++ compose-assignment-1/init.sh
@@ -0,0 +1,7 @@
+# /bin/bash
+
+mkdir -pv -m 777 ./{modules,profiles,themes,sites};
+mkdir -pv -m 777 ./sites/default;
+mkdir -pv -m 777 ./sites/default/files;
+mkdir -pv -m 777 ./sites/default/files/translations;
+#chmod 777 ./sites/default/files/translations;
